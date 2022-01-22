da3488411685b0614bf748123989c420c6264b8b
ktym4a <s19585574@gmail.com>
2022-01-22 11:52:02 +0700

    CREATE POST TODO

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
-from decouple import config
+from typing import Union
+
 import motor.motor_asyncio
+from decouple import config
 
 MONGO_API_KEY = config("MONGO_API_KEY")
 
@@ -7,3 +9,19 @@ client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_API_KEY)
 database = client.API_DB
 collenction_todo = database.todo
 collenction_user = database.user
+
+
+def todo_serializer(todo) -> dict:
+    return {
+        "id": str(todo["_id"]),
+        "title": todo["title"],
+        "description": todo["description"],
+    }
+
+
+async def db_create_todo(data: dict) -> Union[dict, bool]:
+    todo = await collenction_todo.insert_one(data)
+    new_todo = await collenction_todo.find_one({"_id": todo.inserted_id})
+    if new_todo:
+        return todo_serializer(new_todo)
+    return False
