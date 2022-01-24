70dc729fa7a98fb8dbd5f02f9bb0ed828b64dcdc
ktym4a <s19585574@gmail.com>
2022-01-24 13:42:30 +0700

    fix typo

--- routers/route_auth.py
+++ routers/route_auth.py
@@ -16,7 +16,7 @@ def get_csrf_token(csrf_protect: CsrfProtect = Depends()):
     return res
 
 
-@router.post("/api/gesister", response_model=UserInfo)
+@router.post("/api/register", response_model=UserInfo)
 async def signup(request: Request, user: UserBody,
                  csrf_protect: CsrfProtect = Depends()):
     csrf_token = csrf_protect.get_csrf_from_headers(request.headers)
